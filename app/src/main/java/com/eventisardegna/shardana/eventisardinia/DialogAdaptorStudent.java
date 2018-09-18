2a9d6d5dce5e7209e8ff7abb00dff529ddd98084
Pietro Demuro <shardana9699@gmail.com>
2018-09-18 16:26:10 +0200

    luoghi

--- app/src/main/java/com/eventisardegna/shardana/eventisardinia/DialogAdaptorStudent.java
+++ app/src/main/java/com/eventisardegna/shardana/eventisardinia/DialogAdaptorStudent.java
@@ -7,12 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 class DialogAdaptorStudent extends BaseAdapter {
     Activity activity;
 
+    private DatabaseReference databaseReference;
     private Activity context;
     private ArrayList<Dialogpojo> alCustom;
     private String sturl;
@@ -42,23 +49,35 @@ class DialogAdaptorStudent extends BaseAdapter {
 
     @TargetApi(Build.VERSION_CODES.O)
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent){
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.row_addapt, null, true);
+        final View listViewItem = inflater.inflate(R.layout.row_addapt, null, true);
 
         //TextView tvTitle=(TextView)listViewItem.findViewById(R.id.tv_section);
         TextView tvSubject=(TextView)listViewItem.findViewById(R.id.tv_type);
        // TextView tvDuedate=(TextView)listViewItem.findViewById(R.id.tv_desc);
         TextView tvDescription=(TextView)listViewItem.findViewById(R.id.tv_class);
+        Button prenota = (Button) listViewItem.findViewById(R.id.prenota);
 
-
+        prenota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("Prenotazioni").push().setValue(alCustom.get(position).getSubjects());
+            }
+        });
         //tvTitle.setText(alCustom.get(position).getTitles());
         tvSubject.setText(alCustom.get(position).getSubjects());
         //tvDuedate.setText("Due Datefrvrrvrv : "+alCustom.get(position).getDuedates());
         tvDescription.setText(alCustom.get(position).getDescripts());
 
+
         return  listViewItem;
     }
+
 
 }
 
