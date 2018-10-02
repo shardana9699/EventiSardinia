cd6bdd790f4340fb6e2e8c3546fb4daae088dfad
Pietro Demuro <shardana9699@gmail.com>
2018-10-02 16:58:11 +0200

    lista degli eventi

--- app/src/main/java/com/eventisardegna/shardana/eventisardinia/EventAdapter.java
+++ app/src/main/java/com/eventisardegna/shardana/eventisardinia/EventAdapter.java
@@ -1,15 +1,13 @@
 package com.eventisardegna.shardana.eventisardinia;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,17 +19,20 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-class DialogAdaptorStudent extends BaseAdapter {
-    Activity activity;
+public class EventAdapter extends ArrayAdapter<Dialogpojo> {
 
+   /* public EventAdapter(Context context, ArrayList<Dialogpojo>evento){
+        super(context, 0, evento);
+    }
+*/
     private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
     private Activity context;
     private ArrayList<Dialogpojo> alCustom;
     private String sturl;
 
-
-    public DialogAdaptorStudent(Activity context, ArrayList<Dialogpojo> alCustom) {
+    public EventAdapter(Activity context, ArrayList<Dialogpojo> alCustom) {
+        super(context, 0, alCustom);
         this.context = context;
         this.alCustom = alCustom;
 
@@ -44,7 +45,7 @@ class DialogAdaptorStudent extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int i) {
+    public Dialogpojo getItem(int i) {
         return alCustom.get(i);
     }
 
@@ -53,7 +54,6 @@ class DialogAdaptorStudent extends BaseAdapter {
         return i;
     }
 
-    @TargetApi(Build.VERSION_CODES.O)
     @Override
     public View getView(final int position, View convertView, ViewGroup parent){
 
@@ -61,22 +61,17 @@ class DialogAdaptorStudent extends BaseAdapter {
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-            LayoutInflater inflater = context.getLayoutInflater();
-            final View listViewItem = inflater.inflate(R.layout.row_addapt, null, true);
+        LayoutInflater inflater = context.getLayoutInflater();
+        final View listViewItem = inflater.inflate(R.layout.row_addapt, null, true);
 
-            TextView tvSubject = (TextView) listViewItem.findViewById(R.id.tv_type);
-            final TextView tvDescription = (TextView) listViewItem.findViewById(R.id.tv_class);
-            final Button prenota = (Button) listViewItem.findViewById(R.id.prenota);
+        TextView tvSubject = (TextView) listViewItem.findViewById(R.id.tv_type);
+        final TextView tvDescription = (TextView) listViewItem.findViewById(R.id.tv_class);
+        final Button prenota = (Button) listViewItem.findViewById(R.id.prenota);
 
-            final FirebaseUser user = firebaseAuth.getCurrentUser();
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
 
         databaseReference.child("Eventi").child(alCustom.get(position).getSubjects()).child("prenotazioni").addValueEventListener(new ValueEventListener() {
 
-            /**
-             * This method will be invoked any time the data on the database changes.
-             * Additionally, it will be invoked as soon as we connect the listener, so that we can get an initial snapshot of the data on the database.
-             * @param dataSnapshot
-             */
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // get all of the children at this level.
@@ -97,26 +92,23 @@ class DialogAdaptorStudent extends BaseAdapter {
 
 
         });
-            prenota.setOnClickListener(new View.OnClickListener() {
+        prenota.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-                    databaseReference.child("Eventi").child(alCustom.get(position).getSubjects()).child("prenotazioni").push().setValue(user.getUid());
-                    HomeCollection.date_collection_arr=new ArrayList<HomeCollection>();
-                }
+                databaseReference.child("Eventi").child(alCustom.get(position).getSubjects()).child("prenotazioni").push().setValue(user.getUid());
+                HomeCollection.date_collection_arr=new ArrayList<HomeCollection>();
+            }
 
 
-            });
+        });
 
-            tvSubject.setText(alCustom.get(position).getSubjects());
-            tvDescription.setText(alCustom.get(position).getDescripts());
+        tvSubject.setText(alCustom.get(position).getSubjects());
+        tvDescription.setText(alCustom.get(position).getDescripts());
 
         return  listViewItem;
     }
-
-
-
-
 }
+
 
--- app/src/main/java/com/eventisardegna/shardana/eventisardinia/ProfileActivity.java
+++ app/src/main/java/com/eventisardegna/shardana/eventisardinia/ProfileActivity.java
@@ -3,13 +3,16 @@ package com.eventisardegna.shardana.eventisardinia;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
@@ -20,6 +23,7 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,8 +31,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     public GregorianCalendar cal_month, cal_month_copy;
@@ -37,9 +47,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private Button map;
     private Button logout;
     private Button calendario;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference myRef;
-    private ArrayList<Dialogpojo> alCustom;
+    private ArrayList<Dialogpojo> arrayEvento = new ArrayList<Dialogpojo>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,12 +80,40 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         map = (Button) findViewById(R.id.map);
         logout = (Button) findViewById(R.id.logout);
         calendario = (Button) findViewById(R.id.calendario);
+        final ListView eventi = (ListView) findViewById(R.id.row_add);
+
+        databaseReference.child("Eventi").addValueEventListener(new ValueEventListener() {
+
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // get all of the children at this level.
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                // shake hands with each of them.'
+                for (DataSnapshot child : children) {
+                    HomeCollection homeCollection = child.getValue(HomeCollection.class);
+
+                    Dialogpojo evento = new Dialogpojo();
+
+                    evento.setTitles(homeCollection.date);
+                    evento.setDescripts(homeCollection.luogo);
+                    evento.setSubjects(homeCollection.titolo);
+                    EventAdapter eventAdapter = new EventAdapter(ProfileActivity.this, arrayEvento);
+                    eventi.setAdapter(eventAdapter);
+                    eventAdapter.add(evento);
+                    eventAdapter.notifyDataSetChanged();
+
+                }
+            }
+
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         map.setOnClickListener(this);
         logout.setOnClickListener(this);
         calendario.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
